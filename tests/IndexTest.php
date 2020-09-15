acd68a569311f8e3bc3f9d8e65a4e526bccc873e
Cédric Oloa <cedric.oloa@laposte.net>
2020-09-15 11:06:32 +0200

    change test

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -2,6 +2,9 @@
 
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 class IndexTest extends TestCase
 {
@@ -12,12 +15,33 @@ class IndexTest extends TestCase
      */
     public function testHello()
     {
-        $name = 'Cédric';
 
-        ob_start();
-        include __DIR__ . '/../src/pages/hello.php';
-        $content = ob_get_clean();
+        require __DIR__ . '/../vendor/autoload.php';
 
-        $this->assertEquals('Hello Cédric', $content);
+        $request = Request::create(
+            '/hello/Cédric',
+            'GET'
+        );
+
+        $routes = require __DIR__. '/../src/routes.php';
+
+        $context = new RequestContext();
+        $context->fromRequest($request);
+
+        $urlMatcher = new UrlMatcher($routes, $context);
+
+        try {
+            $resultat = ($urlMatcher->match($request->getPathInfo()));
+
+            $request->attributes->add($resultat);
+
+            $response = call_user_func($request->attributes->get('_controller'), $request);
+        } catch (ResourceNotFoundException $e) {
+            $response = new Response('La page demandée n\'exite pas', 404);
+        } catch (Exception $e) {
+            $response = new Response('Une erreur est survenue', 500);
+        }
+
+        $this->assertEquals('Hello Cédric', $response->getContent());
     }
 }
